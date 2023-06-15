58c7aa2b0cdcc6e21e82fb105708bb009cd89253
Deanna Earley <dee@earlsoft.co.uk>
2023-06-15 02:46:34 +0100

    Add comments and constants for the image handling

--- pyBallLib/Image.py
+++ pyBallLib/Image.py
@@ -2,8 +2,9 @@ import struct
 
 from .Constants import Ops, Addr
 
-
 class Image:
+    WORDS_PER_COLUMN = 0x10 # 16x 16-bit words per column
+
     def __init__(self, sequence, index, width, image_filename='', image_data=[], image_bytes=[]):
         self.sequence = sequence
         self.index = index
@@ -22,13 +23,13 @@ class Image:
 
         # Create the data array if not already done
         if len(image_data) == 0:
-            image_data = [0] * (0x10 * 73)
+            image_data = [0] * (self.WORDS_PER_COLUMN * width)
 
         # Validate the array size
-        if len(image_data) != (width * 0x10):
+        if len(image_data) != (self.WORDS_PER_COLUMN * width):
             raise ValueError("Image data size ({size}) does not match expected size ({expected_size})".format(
                 size=len(image_data),
-                expected_size=width * 0x10,
+                expected_size=self.WORDS_PER_COLUMN * width,
             ))
 
         self.data = image_data
@@ -37,22 +38,22 @@ class Image:
         self.height = 73
         self.offset = 0  # Updated when uploaded
 
-        # Generate the pixel map
+        # Generate the pixel map to index each component of each pixel in the column data
         self.pixel_map = []
         offset_counter = 0
         for index in range(73):
             pixel_data = (
                 # Red
-                (offset_counter) % 0x10,
-                int((offset_counter) / 0x10),
+                (offset_counter) % self.WORDS_PER_COLUMN, # Word offset
+                int((offset_counter) / self.WORDS_PER_COLUMN), # Bit position
 
                 # Green
-                (offset_counter + 1) % 0x10,
-                int((offset_counter + 1) / 0x10),
+                (offset_counter + 1) % self.WORDS_PER_COLUMN, # Word offset
+                int((offset_counter + 1) / self.WORDS_PER_COLUMN), # Bit position
 
                 # Blue
-                (offset_counter + 2) % 0x10,
-                int((offset_counter + 2) / 0x10),
+                (offset_counter + 2) % self.WORDS_PER_COLUMN, # Word offset
+                int((offset_counter + 2) / self.WORDS_PER_COLUMN), # Bit position
             )
             self.pixel_map.append(pixel_data)
 
@@ -62,7 +63,7 @@ class Image:
                 offset_counter += 5
 
     def set_pixel(self, x, y, r, g, b):
-        x_offset = (x * 0x10)
+        x_offset = (x * self.WORDS_PER_COLUMN)
         pixel_data = self.pixel_map[y]
 
         # Red
@@ -100,14 +101,14 @@ class Image:
         ))
         # Set the image
         bs = self.sequence.bs()
-        for index in range(0, len(self.data), 0x10):
-            connection.send(Ops.STORE, offset + index, bs, self.data[index:index + 0x10])
+        for index in range(0, len(self.data), self.WORDS_PER_COLUMN):
+            connection.send(Ops.STORE, offset + index, bs, self.data[index:index + self.WORDS_PER_COLUMN])
 
         # Store the offset for the metadata
         self.offset = offset
 
     def upload_col(self, connection, col):
-        x_offset = (col * 0x10)
+        x_offset = (col * self.WORDS_PER_COLUMN)
 
         print("Uploading B{bank}S{sequence}I{image}C{col}".format(
             bank=self.sequence.bank.index,
@@ -117,7 +118,7 @@ class Image:
         ))
         # Set the image column
         connection.send(Ops.STORE, self.offset + x_offset, self.sequence.bs(),
-                        self.data[x_offset:x_offset + 0x10])
+                        self.data[x_offset:x_offset + self.WORDS_PER_COLUMN])
 
     def upload_metadata(self, connection):
         bs = self.sequence.bs()
